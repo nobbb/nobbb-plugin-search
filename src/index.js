c795702ba68c27de941c7208e023360583729f99
AbyChan <crystal.tyan@gmail.com>
2016-07-21 19:43:07 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 'use strict';
 
+const nodejieba = require("nodejieba");
+
 export default class NobbbPluginSearch {
   constructor() {
     this.name = 'search';
+    this.type = 'render';
+
+    nodejieba.load();
+
+    //Inverted index
+    this.keymap = {};
+    this.documentInfoSet = {};
   }
 
-  eachAction() {
+  getName() {
+    return this.name;
+  }
+  
+  getType() {
+    return this.type;
+  }
+
+  eachArticle(rawDocument, documentInfo, cb) {
+    //console.log(rawDocument);
+    let words = nodejieba.cut(rawDocument);
     
+    words.reduce((res, word) => {
+      res[word] = res[word] || 0;
+      res[word]++;
+      return res;
+    }, {});
+    
+    Object.keys(words).forEach(word => {
+      this.keymap[word] = this.keymap[word] || [];
+      this.keymap[word].push({id: documentInfo.id, count: words[word]});
+    });
+    
+    this.documentInfoSet[documentInfo.id] = documentInfo;
+    cb();
   }
 
-  afterAction() {
+  after() {
     
   }
   
